ac6d6203964a46122c9bb519e2ef5d2e154c2633
marcos <lapiramarcos@gmail.com>
2022-09-09 16:05:40 -0300

    formulario hecho en ejercicio2 clase5

---
+++ clase-1/clase5-Entidades/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace clase5_Entidades
+{
+    public class Ingresante
+    {
+        //ATRIBUTOS
+        private string[] cursos;
+        private string direccion;
+        private int edad;
+        private string genero;
+        private string nombre;
+        private string pais;
+
+        public Ingresante(string[] cursos, string direccion, int edad, string genero, string nombre, string pais)
+        {
+            this.cursos = cursos;
+            this.direccion = direccion;
+            this.edad = edad;
+            this.genero = genero;
+            this.nombre = nombre;
+            this.pais = pais;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine();
+            sb.AppendLine(this.direccion);
+            sb.AppendLine(this.edad.ToString());
+            sb.AppendLine(this.genero);
+            sb.AppendLine(this.nombre);
+            sb.AppendLine(this.pais);
+            return sb.ToString();
+        }
+    }
+}
--- clase-1/clase5-ejercicio2/Form1.Designer.cs
+++ clase-1/clase5-ejercicio2/Form1.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbDetallesUsuario = new System.Windows.Forms.GroupBox();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gbGenero = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.gbCursos = new System.Windows.Forms.GroupBox();
+            this.cbJavaScript = new System.Windows.Forms.CheckBox();
+            this.cbCmas = new System.Windows.Forms.CheckBox();
+            this.cbC = new System.Windows.Forms.CheckBox();
+            this.lbPais = new System.Windows.Forms.ListBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.gbDetallesUsuario.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.gbGenero.SuspendLayout();
+            this.gbCursos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbDetallesUsuario
+            // 
+            this.gbDetallesUsuario.Controls.Add(this.lblEdad);
+            this.gbDetallesUsuario.Controls.Add(this.lblApellido);
+            this.gbDetallesUsuario.Controls.Add(this.lblNombre);
+            this.gbDetallesUsuario.Controls.Add(this.numericUpDown1);
+            this.gbDetallesUsuario.Controls.Add(this.textBox2);
+            this.gbDetallesUsuario.Controls.Add(this.textBox1);
+            this.gbDetallesUsuario.Location = new System.Drawing.Point(83, 38);
+            this.gbDetallesUsuario.Name = "gbDetallesUsuario";
+            this.gbDetallesUsuario.Size = new System.Drawing.Size(319, 242);
+            this.gbDetallesUsuario.TabIndex = 0;
+            this.gbDetallesUsuario.TabStop = false;
+            this.gbDetallesUsuario.Text = "Detalles usuario";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(42, 168);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(43, 20);
+            this.lblEdad.TabIndex = 5;
+            this.lblEdad.Text = "Edad";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(41, 94);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(66, 20);
+            this.lblApellido.TabIndex = 4;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(34, 28);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(64, 20);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(34, 188);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(125, 27);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // gbGenero
+            // 
+            this.gbGenero.Controls.Add(this.radioButton3);
+            this.gbGenero.Controls.Add(this.radioButton2);
+            this.gbGenero.Controls.Add(this.radioButton1);
+            this.gbGenero.Location = new System.Drawing.Point(494, 38);
+            this.gbGenero.Name = "gbGenero";
+            this.gbGenero.Size = new System.Drawing.Size(250, 188);
+            this.gbGenero.TabIndex = 1;
+            this.gbGenero.TabStop = false;
+            this.gbGenero.Text = "Genero";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(55, 116);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(117, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(55, 71);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(117, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(55, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(117, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // gbCursos
+            // 
+            this.gbCursos.Controls.Add(this.cbJavaScript);
+            this.gbCursos.Controls.Add(this.cbCmas);
+            this.gbCursos.Controls.Add(this.cbC);
+            this.gbCursos.Location = new System.Drawing.Point(494, 253);
+            this.gbCursos.Name = "gbCursos";
+            this.gbCursos.Size = new System.Drawing.Size(250, 125);
+            this.gbCursos.TabIndex = 4;
+            this.gbCursos.TabStop = false;
+            this.gbCursos.Text = "Cursos";
+            // 
+            // cbJavaScript
+            // 
+            this.cbJavaScript.AutoSize = true;
+            this.cbJavaScript.Location = new System.Drawing.Point(55, 90);
+            this.cbJavaScript.Name = "cbJavaScript";
+            this.cbJavaScript.Size = new System.Drawing.Size(101, 24);
+            this.cbJavaScript.TabIndex = 2;
+            this.cbJavaScript.Text = "Java Script";
+            this.cbJavaScript.UseVisualStyleBackColor = true;
+            // 
+            // cbCmas
+            // 
+            this.cbCmas.AutoSize = true;
+            this.cbCmas.Location = new System.Drawing.Point(55, 60);
+            this.cbCmas.Name = "cbCmas";
+            this.cbCmas.Size = new System.Drawing.Size(60, 24);
+            this.cbCmas.TabIndex = 1;
+            this.cbCmas.Text = "C++";
+            this.cbCmas.UseVisualStyleBackColor = true;
+            // 
+            // cbC
+            // 
+            this.cbC.AutoSize = true;
+            this.cbC.Location = new System.Drawing.Point(55, 26);
+            this.cbC.Name = "cbC";
+            this.cbC.Size = new System.Drawing.Size(49, 24);
+            this.cbC.TabIndex = 0;
+            this.cbC.Text = "C#";
+            this.cbC.UseVisualStyleBackColor = true;
+            // 
+            // lbPais
+            // 
+            this.lbPais.FormattingEnabled = true;
+            this.lbPais.ItemHeight = 20;
+            this.lbPais.Items.AddRange(new object[] {
+            "Argentina",
+            "Chile",
+            "Uruguay"});
+            this.lbPais.Location = new System.Drawing.Point(125, 319);
+            this.lbPais.Name = "lbPais";
+            this.lbPais.Size = new System.Drawing.Size(150, 104);
+            this.lbPais.TabIndex = 5;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(648, 406);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(94, 29);
+            this.btnIngresar.TabIndex = 6;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.lbPais);
+            this.Controls.Add(this.gbCursos);
+            this.Controls.Add(this.gbGenero);
+            this.Controls.Add(this.gbDetallesUsuario);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbDetallesUsuario.ResumeLayout(false);
+            this.gbDetallesUsuario.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.gbGenero.ResumeLayout(false);
+            this.gbGenero.PerformLayout();
+            this.gbCursos.ResumeLayout(false);
+            this.gbCursos.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox gbDetallesUsuario;
+        private Label lblEdad;
+        private Label lblApellido;
+        private Label lblNombre;
+        private NumericUpDown numericUpDown1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private GroupBox gbGenero;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox gbCursos;
+        private CheckBox cbJavaScript;
+        private CheckBox cbCmas;
+        private CheckBox cbC;
+        private ListBox lbPais;
+        private Button btnIngresar;
     }
 }
