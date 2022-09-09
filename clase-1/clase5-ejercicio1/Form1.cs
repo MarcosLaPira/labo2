975f9920e49d77726193537c010d7c5187aef94f
marcos <lapiramarcos@gmail.com>
2022-09-08 21:04:14 -0300

    modificacion en mensaje de error

--- clase-1/clase5-ejercicio1/Form1.cs
+++ clase-1/clase5-ejercicio1/Form1.cs
@@ -11,18 +11,50 @@ namespace clase5_ejercicio1
         {
             string nombre;
             nombre = this.txtNombre1.Text;
+
+            string apellido;
+            apellido = this.textBox2.Text;
+
             string materiaFav;
             materiaFav = this.cbxMateriaFav.Text;
-
-            if (string.IsNullOrEmpty(nombre))//valido si lo ingresado es un nombre
+            /*
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(apellido))//valido si lo ingresado es un nombre
             {
                 //en caso de error muestro mensaje encapsulado de lo siguiente
-                MessageBox.Show("Hay un errror", "ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                MessageBox.Show("Hay un errror, se deben completar nombre y appellido", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
-               Form2 form2 = new Form2(nombre, materiaFav);//instancio form 2
-                form2.ShowDialog();//muestro form2
+              
+            }
+            */
+
+            if (string.IsNullOrEmpty(nombre) && string.IsNullOrEmpty(nombre))//valido si lo ingresado es un nombre y apellido
+            {
+                //en caso de error en ambos
+                MessageBox.Show("Hay un errror, se deben completar nombre y appellido", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(nombre))//en caso de error solo en nombre
+                {
+                    MessageBox.Show("Hay un errror, se debe completar nombre ", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(apellido))//caso de error solo en apellido
+                    {
+                        MessageBox.Show("Hay un errror, se debe completar apellido ", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else//caso de que este ok
+                    {
+                        Form2 form2 = new Form2(nombre, materiaFav);//instancio form 2
+                        form2.ShowDialog();//muestro form2
+                    }
+
+                   
+                }
+
             }
         }
     }
